8e5352ce48f2c8a28426b4838e959baf75929ea5
athallah210505 <n4dhif@gmail.com>
2024-09-04 13:47:06 +0700

    fix error on views

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm ' : '2306275651',
-        'name ' : 'Athallah Nadhif Yuzak ganteng',
-        'class ' : 'PBP B'
+        'npm' : '2306275651',
+        'name' : 'Athallah Nadhif Yuzak ganteng',
+        'class' : 'PBP B'
     }
     return render(request, 'main.html', context)
