a72b97231309095812affac36b7cca5d74801073
athallah210505 <n4dhif@gmail.com>
2024-10-02 14:18:05 +0700

    menyelesaikan Tutorial 5

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import show_main
-from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id,add_mood_entry_ajax
 from . import views
 from main.views import register
 from main.views import login_user
@@ -11,7 +11,7 @@ app_name = 'main'
 
 urlpatterns = [
     path ('', views.show_main, name='show_main'),
-    path ('create_mood_entry', create_mood_entry, name='create_mood_entry'),
+    path ('create-mood-entry', create_mood_entry, name='create_mood_entry'),
     path ('xml/', show_xml, name='show_xml'),
     path ('json/', show_json, name='show_json'),
     path ('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'), 
@@ -19,7 +19,8 @@ urlpatterns = [
     path ('register/', register, name='register'),
     path ('login/', login_user, name='login'),
     path ('logout/', logout_user, name='logout'),
-    path ('edit_mood/<uuid:id>/', edit_mood, name='edit_mood'),
-    path ('delete_mood/<uuid:id>/', delete_mood, name='delete_mood'),
+    path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
+    path('delete/<uuid:id>', delete_mood, name='delete_mood'),
+    path ('add-mood-entry-ajax/', add_mood_entry_ajax, name='add_mood_entry_ajax'),
     
 ]
